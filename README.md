887ba1da27dab725c11eb61a0c200ab993bc1cf8
Shwetachudasama <46615961+Shwetachudasama@users.noreply.github.com>
2021-04-15 20:01:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Weather-predictor
+# Weather-Scraper
+I made this app to practice some basic backend coding with PHP. The app is built with HTML, CSS, Bootstrap, JavaScript, jQuery, AJAX, and PHP. On this web app the user can search for the 3 day weather forecast of any city in the world. This is accomplished by using PHP and Regex to use the value of the user's form input entry to search a site for the weather and scrape the 3 day forecast. Then the data is added to an alert on the page using jQuery and AJAX.
+
+---
+
+- This is the first thing the user will see when the app loads. There is the app name "Weather Finder" and brief instructions for what to do. There is a form text input for the user to enter a city name and a simple submit button.
+![INTRO](assets/img/read_me/intro.png)
+
+- Once the user enters a city to search for the app will display the 3 Day Forecast for that city in a Bootstrap alert underneath the search form. This example uses the city of Los Angeles, California.
+![LOS ANGELES](assets/img/read_me/los-angeles.png)
+
+- Here is another example using the city of Chattanooga, Tennessee
+![CHATTANOOGA](assets/img/read_me/chattanooga.png)
+
+- When a user mispells a city they will recieve a Bootstrap alert to try the search again.
+![TYPO](assets/img/read_me/typo.png)
+
+- When a user submits a search without filling out the form field they will recieve a Bootstrap alert to enter a city and search again.
+![NO SEARCH](assets/img/read_me/no-search.png)
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<!-- PAGE TITLE -->
+	<title>Weather Scraper</title>
+
+	<!-- FAVICON -->
+	<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+
+	<!-- META DATA -->
+  <meta charset="utf-8" />
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet">
+
+	<!-- RESET CSS LINK -->
+  <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+
+	<!-- CUSTOM CSS LINK -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	
+	<!-- BOOTSTRAP CDN -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+	<!-- WEATHER INFO PANEL -->
+	<div class="container">
+		<div class="row text-center">
+			<div id="weather-panel" class="col-12 col-sm-10 col-md-8">
+
+				<!-- APP NAME/TITLE -->
+				<h1>WEATHER FINDER</h1>
+				<!-- APP INSTRUCTIONS/INFO -->
+				<p>Search any City name to find the current weather anywhere in the World!</p>
+
+				<!-- CITY SEARCH FORM -->
+				<form method="post">
+					<div class="form-group">
+						<input type="text" name="city" id="city" class="form-control" placeholder="Eg. London, Hong Kong, New York..." />
+					</div>
+
+					<button type="submit" id="find-weather" class="btn btn-primary btn-lg">Submit</button>
+				</form>
+				<!-- END CITY SEARCH FORM -->
+
+				<!-- SUCCESSFUL WEATHER SEARCH ALERT -->
+				<div class="alert alert-primary" role="alert">
+					<p>Three Day Weather Forecast:</p>
+					<span id="forecast"></span>
+				</div>
+
+				<!-- ERROR WEATHER SEARCH ALERT -->
+				<div class="alert alert-danger" role="alert"></div>
+
+			</div>
+		</div>
+	</div>
+	<!-- END WEATHER INFO PANEL -->
+
+
+<!-- JQUERY CDN -->
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
+<!-- CUSTOM APP JS LINK -->
+<script type="text/javascript" src="assets/js/app.js"></script>
+
+</body>
+</html>
